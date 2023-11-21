f3cc0624ef94da248ce57b98da9c30a070e9d4f0
phuc <40540902+phucprime@users.noreply.github.com>
2023-11-21 08:18:16 +0700

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -18,18 +18,8 @@ tar -xf $SOURCE_TAR -C flipper --strip-components=1
 # Apply patches
 pushd flipper
 git init && git add . && git commit -m "Original source"
-
-
 git remote add flipperUpstream https://github.com/facebook/flipper
 git fetch flipperUpstream pull/3553/head:universalBuild
-
-git cherry-pick --keep-redundant-commits \
-6cc083b860bb156592dfc622174e6f8d780a6b97 \
-2216a3f94db296290b855f036c80a6643d20e884 \
-1e1cc37c3bff012f6b9f8c376baeb4675f6dfa29 \
-b26997af00b3779dc8fc65e889b87312ae21fea5 \
-bd477678bc7698b16339aa7f8957f29811cbb0ab \
-ed6e15edc8bd4e4c496088d76e46aeea724a4375 
 
 pushd desktop
 if grep -Fxq "@electron/universal" package.json
@@ -38,7 +28,7 @@ then
 else
     echo "Patching electron/universal in the resolutions"
     resolutions='"resolutions": {'
-    electron_resolution='"@electron/universal": "1.3.4",'
+    electron_resolution='"@electron/universal": "2.0.0",'
     sed -i '' "/$resolutions/ a\\
     $electron_resolution
     " package.json
@@ -53,4 +43,4 @@ sed -i '' "/$build_info/ a\\
 " package.json
 
 yarn install
-yarn build --mac-dmg
+yarn build --mac
